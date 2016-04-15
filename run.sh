7dda84fbc5bd6576193525dc9e096e3eeba9ad0a
rickarm <rickarm@gmail.com>
2016-04-15 12:41:53 -0700

    For loop working

--- run.sh
+++ run.sh
@@ -1,8 +1,25 @@
-sh init.sh
+#!/bin/sh
+
+# Test Account
+# curl https://api.nylas.com/account -u $TOKEN:
 
 #curl https://api.nylas.com/threads?limit=1 -u $TOKEN: > threads.json
 #curl https://api.nylas.com/threads?limit=1 -u $TOKEN: > messages.json
 #curl https://api.nylas.com/calendars?limit=1 -u $TOKEN: > calendar.json
-curl "https://api.nylas.com/contacts?limit=1000&offset=0" -u $TOKEN: > ./export/contacts0.json
-#curl https://api.nylas.com/contacts?limit=1000&offset=1000 -u $TOKEN: > ./export/contacts1.json
-#curl https://api.nylas.com/contacts?limit=1000&offset=2000 -u $TOKEN: > ./export/contacts2.json
+
+# GET CONTACTS
+curl https://api.nylas.com/contacts?view=count -u $TOKEN:
+#curl "https://api.nylas.com/contacts?limit=1000&offset=0" -u $TOKEN: > ./export/contacts0.json
+#curl "https://api.nylas.com/contacts?limit=1000&offset=1000" -u $TOKEN: > ./export/contacts1.json
+#curl "https://api.nylas.com/contacts?limit=1000&offset=2000" -u $TOKEN: > ./export/contacts2.json
+#curl "https://api.nylas.com/contacts?limit=1000&offset=3000" -u $TOKEN: > ./export/contacts3.json
+#curl "https://api.nylas.com/contacts?limit=1000&offset=3000" -u $TOKEN: > ./export/contacts3.json
+
+
+for i in {1..24}
+do
+  (( OFFSET = ($i-1)*1000 ))
+  #echo $OFFSET
+  curl "https://api.nylas.com/contacts?limit=1000&offset=$OFFSET" -u $TOKEN: > ./export/contacts$i.json
+  # echo "Welcome $i times"
+done
