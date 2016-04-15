d36ca02c81a486b754485d7c176fd6249161ed2a
Rick Armbrust <rickarm@gmail.com>
2016-04-15 10:44:18 -0700

    Initial push

---
+++ run.sh
@@ -0,0 +1,7 @@
+export TOKEN=y1NbHe22lQ018BmtJRGf5U8HqWLsoZ
+#curl https://api.nylas.com/threads?limit=1 -u $TOKEN: > threads.json
+#curl https://api.nylas.com/threads?limit=1 -u $TOKEN: > messages.json
+#curl https://api.nylas.com/calendars?limit=1 -u $TOKEN: > calendar.json
+curl "https://api.nylas.com/contacts?limit=1000&offset=0" -u $TOKEN: > ./export/contacts0.json
+#curl https://api.nylas.com/contacts?limit=1000&offset=1000 -u $TOKEN: > ./export/contacts1.json
+#curl https://api.nylas.com/contacts?limit=1000&offset=2000 -u $TOKEN: > ./export/contacts2.json
