47c3feeaceb65bb37671dc39ad8f855486b2244e
rickarm <rickarm@gmail.com>
2016-04-15 17:13:24 -0700

    v1 done

--- run.sh
+++ run.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
 
-# Test Account
+## TEST
 # curl https://api.nylas.com/account -u $TOKEN:
 
-#curl https://api.nylas.com/threads?limit=1 -u $TOKEN: > threads.json
-#curl https://api.nylas.com/threads?limit=1 -u $TOKEN: > messages.json
-#curl https://api.nylas.com/calendars?limit=1 -u $TOKEN: > calendar.json
+## GET CONTACTS
+((n = 28))  # Ideally programatically generated with curl https://api.nylas.com/contacts?view=count -u $TOKEN:
 
-# GET CONTACTS
-curl https://api.nylas.com/contacts?view=count -u $TOKEN:
-#curl "https://api.nylas.com/contacts?limit=1000&offset=0" -u $TOKEN: > ./export/contacts0.json
-#curl "https://api.nylas.com/contacts?limit=1000&offset=1000" -u $TOKEN: > ./export/contacts1.json
-#curl "https://api.nylas.com/contacts?limit=1000&offset=2000" -u $TOKEN: > ./export/contacts2.json
-#curl "https://api.nylas.com/contacts?limit=1000&offset=3000" -u $TOKEN: > ./export/contacts3.json
-#curl "https://api.nylas.com/contacts?limit=1000&offset=3000" -u $TOKEN: > ./export/contacts3.json
-
-
-for i in {1..24}
+for i in $(seq 1 $n)
 do
-  (( OFFSET = ($i-1)*1000 ))
-  #echo $OFFSET
-  curl "https://api.nylas.com/contacts?limit=1000&offset=$OFFSET" -u $TOKEN: > ./export/contacts$i.json
-  # echo "Welcome $i times"
+  (( CONTACTS_OFFSET = ($i-1)*1000 ))
+  curl "https://api.nylas.com/contacts?limit=1000&offset=$CONTACTS_OFFSET" -u $TOKEN: > ./export/contacts$i.json
 done
+
+## GET CALENDAR
+((n = 4))  # Ideally programatically generated with curl https://api.nylas.com/events?view=count -u $TOKEN:
+
+for i in $(seq 1 $n)
+do
+  (( CAL_OFFSET = ($i-1)*1000 ))
+  curl "https://api.nylas.com/events?limit=1000&offset=$CAL_OFFSET" -u $TOKEN: > ./export/calendar$i.json
+done
+
+## GET THREADS
+((n = 62))   # Ideally programatically generated with curl https://api.nylas.com/threads?view=count -u $TOKEN:
+
+for i in $(seq 1 $n)
+do
+  (( THREAD_OFFSET = ($i-1)*1000 ))
+  curl "https://api.nylas.com/threads?view=expanded&limit=1000&offset=$THREAD_OFFSET" -u $TOKEN: > ./export/thread$i.json
+done
+
+## GET MESSAGES
+#curl "https://api.nylas.com/messages?view=expanded&limit=1" -u $TOKEN: #> messages.json
